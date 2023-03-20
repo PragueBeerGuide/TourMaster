3adb9f9f42c1f49c4090a00daa4408eee25ed93c
PragueBeerGuide <vondra.ctu@gmail.com>
2023-03-20 16:45:12 +0100

    admins, permissions migrations created

---
+++ database/migrations/2023_03_20_153240_create_admins_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->bigIncrements('id'); 
+            $table->foreignId('permission_id');
+            $table->string('fullname');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
