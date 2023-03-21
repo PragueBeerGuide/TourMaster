39183784ecdab4737b25d66d706a5aef4d365232
Klaus <kriegle@seznam.cz>
2023-03-21 09:45:16 +0100

    merch

--- database/migrations/2023_03_20_104336_create_merchandise_table.php
+++ database/migrations/2023_03_20_104336_create_merchandise_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('merchandise', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->timestamps();
+            $table->timestamps();             
         });
     }
 
