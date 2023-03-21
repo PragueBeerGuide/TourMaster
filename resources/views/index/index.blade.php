cda50e4d1ba3ca389a77ea68c36403ffa1ac1d6b
Klaus <kriegle@seznam.cz>
2023-03-21 14:25:43 +0100

    merch

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Prague Beer Tours</title>
+</head>
+<body>
+    <nav class="header">
+        <ul class="header_links" style="Display:Flex; justify-content: space-between; align-items: center">
+            <li class="header_links-select">Private Tours</li>
+            <li class="header_links-select">Book a Tour</li>
+            <li class="header_links-select">Gift Cards</li>
+            <li class="header_links-select">Blog</li>
+            <li class="header_links-select">About Us</li>
+        </ul>
+    </nav>
+<img src="images/stand-in.JPG" alt="">
+    <div class="mid" style="Display:Flex; justify-content: space-between; flex-wrap:wrap; padding: 2rem">
+        <div class="mid_item">
+            <span class="mid_item_image">&lt;</span>
+            <span>3-3 1/2 hours</span>
+        </div>
+        <div class="mid_item">
+            <span class="mid_item_image">&lt;</span>
+            <span>6 stops</span>
+        </div>
+        <div class="mid_item">
+            <span class="mid_item_image">&lt;</span>
+            <span>12 people max</span>
+        </div>
+        <div class="mid_item">
+            <span class="mid_item_image">&lt;</span>
+            <span>English</span>
+        </div>
+        <div class="mid_item">
+            <span class="mid_item_image">&lt;</span>
+            <span>Local Food & Culture</span>
+        </div>
+        <div class="mid_item">
+            <span class="mid_item_image">&lt;</span>
+            <span>Award-winning</span>
+        </div>
+    </div>
+    <main style="Display: flex">
+        <div class="info">
+        <hr>
+        <h2>What you'll do:</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quaerat maiores voluptatum perspiciatis repudiandae iure modi asperiores natus ipsam recusandae pariatur nemo tempore, expedita reiciendis voluptatibus error ipsum. Libero, officiis.</p>
+        <ul>
+            <li>first, you'll go here</li>
+            <br>
+            <br>
+            <li>first, you'll go here</li>
+            <br>
+            <br>
+            <li>first, you'll go here</li>
+            <br>
+            <br>
+            <li>first, you'll go here</li>
+            <br>
+            <br>
+            <li>first, you'll go here</li>
+            <br>
+            <br>
+            <li>first, you'll go here</li>
+            <br>
+            <br>
+            <li>first, you'll go here</li>
+        </ul>
+    </div>
+    <img src="images/calendar.JPG" alt="">
+    </main>
+    <div class="what" style="width: 20%; display: flex; justify-content:space-around">
+        <div class="what__food">
+        <h2>The Food</h2>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+    </div>
+        <div class="what__food">
+        <h2>Drinky</h2>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+        </div>
+    </div>
+    <div class = "why">
+        
+    </div>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\index;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/beer-tours', [index::class, 'front']);
