219d38525c8995f78a2b5dabef1ed3a5064d2c9d
Klaus <kriegle@seznam.cz>
2023-03-23 13:18:26 +0100

    calendar

--- resources/js/src/App.jsx
+++ resources/js/src/App.jsx
@@ -1,17 +1,33 @@
 import Calendar from 'react-calendar';
+import React, { useState } from 'react';
 import { Hero } from '../components/header/Header';
-import StickyNavbar from '../components/navbar/Navbar'
-
+import "../../css/app.css";
 
 
 function App() {
-
+const [value, onChange] = useState(new Date())
   return (
-
-    <div className="App">
-      <StickyNavbar/>
+<>
+<div className="Sample">
+      <header>
+        <h1>react-calendar sample page</h1>
+      </header>
+      <div className="Sample__container">
+        <main className="Sample__container__content">
+          <Calendar onChange={onChange} showWeekNumbers value={value} />
+        </main>
+      </div>
+    </div>
+    {/* <div className="App">
+      <Hero/>
+    
       <Calendar/>
     </div>
+
+<h1 className="text-4xl font-bold underline">
+Hello world!
+</h1> */}
+</>
   )
 }
 
