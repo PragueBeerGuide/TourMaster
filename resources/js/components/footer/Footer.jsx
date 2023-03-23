d4d25ffcd0f5a43599efa0c452f2c9902fbff600
Klaus <kriegle@seznam.cz>
2023-03-23 16:22:09 +0100

    working calendar

--- resources/js/components/footer/Footer.jsx
+++ resources/js/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-export function footer() {
+export default function Footer() {
     return (
         <>
             <footer>
--- resources/js/src/App.jsx
+++ resources/js/src/App.jsx
@@ -1,12 +1,18 @@
-import "../../css/app.css";
-
+import StickyNavbar from "../components/navbar/StickyNavbar";
+import { Booking } from "../components/calendars/Calendar";
 function App() {
 
   return (
-
-    <h1 className="text-4xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <StickyNavbar />
+      <Booking/>
+      <div className="bg-red-400">
+          <h1 className="text-4xl font-bold underline">
+            Hello world!
+          </h1>
+          <p className="text-slate-600 text-xl">I wish this Tailwinf styling would finally work....</p>
+      </div>
+    </>
   )
 }
 
