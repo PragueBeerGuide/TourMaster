187d7b844ec5ca4b4913f13bc721584b3bc333c6
PragueBeerGuide <vondra.ctu@gmail.com>
2023-03-23 13:11:47 +0100

    Tailwind finally fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [ 
     "./resources/**/*.{blade.php, js, jsx}",
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.jsx",
+    "./resources/js/src/App.jsx",
+    "./resources/views/welcome.blade.php",
+    "./index.html",
+    "./src/**/*.{js, jsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+});
