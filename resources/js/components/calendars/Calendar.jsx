8aa6d0f226f3172b28e3b79287a66c6f459d14b2
Klaus <kriegle@seznam.cz>
2023-03-24 13:08:27 +0100

    button functions

--- resources/js/components/calendars/Calendar.jsx
+++ resources/js/components/calendars/Calendar.jsx
@@ -1,32 +1,45 @@
 import { useState } from "react";
 import React from "react";
 import { Calendar } from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
+import "react-calendar/dist/Calendar.css";
 import DatePicker from "react-date-picker";
-import moment from 'moment';
+import moment from "moment";
 import ListOfEvents from "./ListOfEvents";
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+    Tooltip,
+} from "@material-tailwind/react";
 
+export function Booking() {
+    const [dateState, setDateState] = useState(new Date());
+    const [openDetails, setOpenDetails] = useState(false);
 
-
-
-export function Booking(){
-    const [dateState, setDateState] = useState(new Date())
-    
     const changeDate = (e) => {
-        setDateState(e)
-      }
-return (
-    <div className="w-full max-w-[30rem] shadow-lg">
-        <Calendar
-            value = {dateState} 
-            onChange = {changeDate}  
-            minDate = {new Date()} 
-            minDetail = 'month'
-        />
-          
-        <p>You've selected <b>{moment(dateState).format('MMMM Do YYYY')}</b></p>
+        setDateState(e);
+        setOpenDetails(e);
+    };
 
-        <ListOfEvents />
-    </div>
-    )
+    return (
+        <div className="w-full max-w-[30rem] shadow-lg">
+            <Calendar
+                value={dateState}
+                onChange={changeDate}
+                minDate={new Date()}
+                minDetail="month"
+            />
+
+            <p>
+                You've selected{" "}
+                <b>{moment(dateState).format("MMMM Do YYYY")}</b>
+            </p>
+
+            {openDetails ? <ListOfEvents /> : null}
+            
+        </div>
+    );
 }
