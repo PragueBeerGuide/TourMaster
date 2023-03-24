7001d5321c0e2ccf13e0a067562f5886636e16b9
Klaus <kriegle@seznam.cz>
2023-03-24 09:51:42 +0100

    calendar modifications

--- resources/js/components/calendars/Calendar.jsx
+++ resources/js/components/calendars/Calendar.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import React from "react";
 import { Calendar } from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
-
+import DatePicker from "react-date-picker";
+import moment from 'moment';
 
 
 
 
 export function Booking(){
     const [dateState, setDateState] = useState(new Date())
+    
 
     const changeDate = (e) => {
         setDateState(e)
@@ -16,6 +18,8 @@ export function Booking(){
 return (
     <>
           <Calendar  value={dateState} onChange={changeDate} />
+          
+          <p>You've selected <b>{moment(dateState).format('MMMM Do YYYY')}</b></p>
    </>
 
 )
