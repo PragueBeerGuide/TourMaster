c2aded6b60e36eb4fcfdc9db00763dff42af1ae2
Martin Dedic <m.dedic99@seznam.cz>
2023-03-27 09:27:02 +0200

    showing only relevant users info

--- resources/views/auth/home.blade.php
+++ resources/views/auth/home.blade.php
@@ -9,13 +9,30 @@
 @endphp
 
 @if ($user->permission_id === 1)
-    @foreach ($users as $user)
-    <div>
-        <li>{{ $user->name }}</li>
-        <li>{{ $user->email }}</li>
-        <li>{{ !$user->permission_id ? 'Null' : $user->permission_id }}</li>
-        <button onclick="window.location.href='{{route('show-admin', ['id' => $user->id])}}'">Edit</button>
-    </div>
+    @foreach ($users as $currentUser)
+        @if ($currentUser->permission_id === 1)
+            @continue
+        @endif
+        <div>
+            <li>{{ $currentUser->name }}</li>
+            <li>{{ $currentUser->email }}</li>
+            <li>{{ !$currentUser->permission_id ? 'Null' : $currentUser->permission_id }}</li>
+            <button onclick="window.location.href='{{route('show-admin', ['id' => $currentUser->id])}}'">Edit</button>
+        </div>
+    @endforeach
+@endif
+
+@if ($user->permission_id === 2)
+    @foreach ($users as $currentUser)
+        @if ($currentUser->permission_id === 1 || $currentUser->permission_id === 2)
+            @continue
+        @endif
+        <div>
+            <li>{{ $currentUser->name }}</li>
+            <li>{{ $currentUser->email }}</li>
+            <li>{{ !$currentUser->permission_id ? 'Null' : $currentUser->permission_id }}</li>
+            <button onclick="window.location.href='{{route('show-admin', ['id' => $currentUser->id])}}'">Edit</button>
+        </div>
     @endforeach
 @endif
 
