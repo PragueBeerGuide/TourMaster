2c9a632918855120bdfc1a22d343de18297848ca
Klaus <kriegle@seznam.cz>
2023-03-31 12:19:50 +0200

    calendar fixes

--- resources/js/components/calendars/Calendar.jsx
+++ resources/js/components/calendars/Calendar.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import React from "react";
 import { Calendar } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import DatePicker from "react-date-picker";
+
 import moment from "moment";
 import ListOfEvents from "./ListOfEvents";
-import { useEffect } from "react";
+
 import {
     Card,
     CardHeader,
@@ -15,10 +15,10 @@ import {
     Button,
     Tooltip,
 } from "@material-tailwind/react";
-import Form from "../Form/UseMultistepForm";
+
 import { useParams } from "react-router-dom";
-import axios from "axios";
-import TourSummary from "../tour/TourSummary";
+
+
 
 
 export default function CustomCalendar({tourTitle}) {
@@ -26,35 +26,19 @@ export default function CustomCalendar({tourTitle}) {
     const [events, setEvents] = useState([]);
     const [dateState, setDateState] = useState(new Date());
     const [openDetails, setOpenDetails] = useState(false);
-    const formatedDate = moment().format("Y-MM-DD HH:mm:ss").split(" ")[0];
-    const getData = async () => {
-        
-        try {
-            console.log('making request', `${window.location.origin}/events/${tourId}/`);
-            const response = await axios.get(
-                `${window.location.origin}/events/${tourId}/${formatedDate}`
-            );
-            console.log(response.data)
-            setEvents(response.data);
-        } catch (error) {
-            console.log(error);
-            console.log(error.response);
-        }
-    };
+  
+ 
 
     const changeDate = (date) => {
         setDateState(date);
         setOpenDetails(true);
     };
-    useEffect(() => {
-        getData();
-        console.log(events);
-    }, []);
+
 
     return (
         <div className="flex justify-center">
             <div className="w-full max-w-[30rem] shadow-lg flex flex-col items-center py-12 m-10">
-            You've selected {tourTitle}
+          
                 <Button
                     size="lg"
                     className="flex justify-center items-center gap-4 bg-red-700"
